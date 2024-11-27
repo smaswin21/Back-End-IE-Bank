2afe5ab61a20cedd95a8e1b08778b53df27c0284
Aswin Subramanian Maheswaran <130904493+smaswin21@users.noreply.github.com>
2024-11-27 16:30:45 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    APPINSIGHTS_CONNECTION_STRING = os.getenv('APPINSIGHTS_CONNECTION_STRING')  # Get the full Application Insights connection string from the environment
-    APPINSIGHTS_INSTRUMENTATIONKEY = os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')  # Get the Instrumentation Key from the environment
+    APPINSIGHTS_CONNECTION_STRING = os.getenv('APPINSIGHTS_CONNECTION_STRING')
+    APPINSIGHTS_INSTRUMENTATIONKEY = os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')
+
 class LocalConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
     DEBUG = True
@@ -21,8 +22,8 @@ class GithubCIConfig(Config):
 class DevelopmentConfig(Config):
     if os.getenv('ENV') == 'dev':
         credential = DefaultAzureCredential()
-        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-            dbuser=urllib.parse.quote(os.getenv('DBUSER')),
+        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}?sslmode=require'.format(
+            dbuser=urllib.parse.quote('b60ac699-813d-45f7-a8b6-4080578c9f29'),
             dbpass=credential.get_token('https://ossrdbms-aad.database.windows.net/.default').token,
             dbhost=os.getenv('DBHOST'),
             dbname=os.getenv('DBNAME')
@@ -32,7 +33,7 @@ class DevelopmentConfig(Config):
 class UATConfig(Config):
     if os.getenv('ENV') == 'uat':
         credential = DefaultAzureCredential()
-        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}?sslmode=require'.format(
             dbuser=urllib.parse.quote(os.getenv('DBUSER')),
             dbpass=credential.get_token('https://ossrdbms-aad.database.windows.net/.default').token,
             dbhost=os.getenv('DBHOST'),
